f995669c4f59e4071072cb230cc65689add7465d
Ngan Kim Khong <ngankhong03@gmail.com>
2020-01-15 04:36:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -225,7 +225,7 @@ def winning_team
   game_hash.each do |place, team|
     team_score[team[:team_name]] = 0
     team.each do |attribute, data|
-      team_score[team[:team_name]] = team[:players][:points] +
+      
     end
   end
   best_team
